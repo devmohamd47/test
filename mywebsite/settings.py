719d5ab9615f00731ca45c041610ef9c6e98f64c
Dev. Mohamd <devmohamd@gmail.com>
2023-06-25 13:07:01 +0300

    new

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -119,13 +119,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/res/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "res/"),
-)
+STATIC_URL = 'res/'
+# STATICFILES_DIRS = [BASE_DIR/'static']
+
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'res'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'res')
 
 
-# Default primary key field type
+# Default primary key field type 
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
